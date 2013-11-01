c2d23453d5bcea53576f69c24396d82affe806bb
wakamsha <wakamsha@gmail.com>
2013-11-02 00:57:57 +0900

    基本的な動作の実装

--- middleman/source/assets/js/20/togglePasswordVisible.js
+++ middleman/source/assets/js/20/togglePasswordVisible.js
@@ -1,51 +1,80 @@
 (function($) {
 
-	var TogglePasswordVisible = function(element, opts){
+	// デフォルトオプション
+	var defaults = {
+		show: 'unclear',
+		toggleEvent: 'click',
+		states: {
+			shown: {
+				eventName: 'passwordShown',
+				inputClass: 'toggle-password-visible-shown',
+				toggleClass: 'toggle-password-visible-hide',
+				attr: {
+					type: 'text',
+					autocapitalize: 'off',
+					autocomplete: 'off',
+					spellcheck: false
+				}
+			},
+			hidden: {
+				eventName: 'passwordHidden',
+				inputClass: 'toggle-password-visible-hidden',
+				toggleClass: 'toggle-password-visible-show',
+				attr: {
+					type: 'password'
+				}
+			}
+		}
+	};
+
+	var TogglePasswordVisible = function(element, options){
 		this.element = $(element);
-		this.init(opts);
+		this.init(options);
 	};
 
 	TogglePasswordVisible.prototype = {
-		// デフォルトオプション
-		defaults: {
-			show: false,
-			toggleEvent: 'click',
-			states: {
-				shown: {
-					attr: {
-						type: 'text',
-						autocapitalize: 'off',
-						autocomplete: 'off',
-						autocorrect: 'off',
-						spellcheck: false
-					}
-				},
-				hidden: {
-					attr: {
-						type: 'password'
-					}
-				}
-			}
-		},
 		
-		init: function(opts) {
-			console.log('init');
-			this.update(opts);
-			// this.initInnerToggle(this.element, opts);
+		init: function(options) {
+			this.update(options, defaults, (this.element.attr('type') === 'password'));
+			this.initInnerToggle(this.element, this.options);
 		},
 
-		update: function(opts, optsBase) {
-			optsBase = optsBase || this.opts;
-			if (typeof opts !== 'Object') {
-				opts = {show: opts};
+		update: function(options, optionsBase, toggleFallback) {
+			optionsBase = optionsBase || this.options;
+			toggleFallback = toggleFallback || !this.options.show;
+			if (typeof options !== 'Object') {
+				options = {show: options};
 			}
 			// オプションをマージ
-			this.opts = $.extend({}, optsBase, opts);
-			
-			console.log(opts);
+			this.options = $.extend({}, optionsBase, options);
+
+			if (this.options.show === 'unclear') {
+				this.options.show = false;
+			}
+			if (this.options.show === 'toggle') {
+				this.options.show = toggleFallback;
+			}
+
+			// 状態を適用する
+			var currentKey = this.currentStateKey();
+
+			$.each(this.options.states, $.proxy(function(key, state) {
+				if (currentKey === key) {
+					$.each(state.attr, $.proxy(function(key, value){
+						this.element.attr(key, value);
+					}, this));
+					this.element.addClass(state.inputClass).trigger(state.eventName);
+				} else {
+					this.element.removeClass(state.inputClass).trigger(state.eventName);
+				}
+			}, this));
 		},
 
-		initInnerToggle: function(el, opts) {
+		currentStateKey: function() {
+			return this.options.show ? 'shown' : 'hidden';
+		},
+
+		initInnerToggle: function(element, options) {
 			var wrapper,
 				toggle,
 				wrapperCSS = {
@@ -58,21 +87,36 @@
 					bottom: 0,
 					width: '30px',
 					backgroundColor: 'red'
-				};
-			el.wrap('<div class="toggle-password-visible-wrapper"/>');
-			wrapper = el.parent();
+				},
+				eventName = '';
+
+			element.wrap('<div class="toggle-password-visible-wrapper"/>');
+			wrapper = element.parent();
 			wrapper.css(wrapperCSS);
 			toggle = $('<div class="toggle-password-visible-toggle"/>').css(toggleCSS);
 
 			toggle.appendTo(wrapper);
 
-			toggle.on(opts.toggleEvent, $.proxy(function(event) {
+			toggle.on(options.toggleEvent, $.proxy(function(event) {
 				this.update('toggle');
+			}, this));
+
+			$.each(this.options.states, function(key, state) {
+				eventName+= state.eventName + ' ';
+			});
+			element.on(eventName, $.proxy(function() {
+				this.updateInnerToggle(toggle, this.currentStateKey(), options)
 			}, this));
 		},
 
-		updateInnerToggle: function(el, current) {
-
+		updateInnerToggle: function(element, currentKey, states) {
+			$.each(this.options.states, function(key, state) {
+				if (currentKey == key) {
+					element.addClass(state.toggleClass);
+				} else {
+					element.removeClass(state.toggleClass);
+				}
+			});
 		}
 	};
 
